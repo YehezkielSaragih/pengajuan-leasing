21c7393b61afdc2962bfe75936948af96fc3437a
Yehezkiel Stephanus Austin <yehezkiel.stephanus@gmail.com>
2025-09-17 16:09:23 +0700

    feat: add login and register page (no logic)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { FormApplicationComponent } from './pages/application-form/application-f
 import { ApplicationStatusComponent } from './pages/application-status/application-status.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ApplicationDetailComponent } from './pages/application-detail/application-detail.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'form-application', pathMatch: 'full' },
   { path: 'form-application', component: FormApplicationComponent },
   { path: 'application-status', component: ApplicationStatusComponent },
   { path: 'application-status/:id', component: ApplicationDetailComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '404' }
 ];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+}
