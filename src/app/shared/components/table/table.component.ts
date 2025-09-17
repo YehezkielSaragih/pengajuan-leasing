30de7d7820bec10b72a3cdee284de845a28af9e1
Rian Adriansyah <rian.adriansyah@fifgroup.astra.co.id>
2025-09-17 11:56:34 +0700

    feat: fix table component and add sidebar

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component ,Input, Output, EventEmitter} from '@angular/core';
+import { Application } from '../../../models/application.model';
+
 
 @Component({
-  selector: 'app-table',
+  selector: 'shared-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
 export class TableComponent {
+  @Input() applications: Application[] = [];      // menerima data
+  @Output() delete = new EventEmitter<number>();   // emit event delete
+
+  onDelete(id: number): void {
+    this.delete.emit(id);   // emit ke parent
+  }
 
 }
