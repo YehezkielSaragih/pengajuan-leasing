941803579b472dc3b957bbd7ba766e53af15230e
Rian Adriansyah <rian.adriansyah@fifgroup.astra.co.id>
2025-09-17 09:31:44 +0700

    create pages

---
+++ src/app/pages/form-application/form-application.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormApplicationComponent } from './form-application.component';
+
+describe('FormApplicationComponent', () => {
+  let component: FormApplicationComponent;
+  let fixture: ComponentFixture<FormApplicationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FormApplicationComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FormApplicationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/form-application/form-application.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-application',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-application.component.html',
+  styleUrl: './form-application.component.scss'
+})
+export class FormApplicationComponent {
+
+}
