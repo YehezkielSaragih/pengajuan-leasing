751584c02368aa4082ef7c4c26acc4d7ce32fb0a
Rian Adriansyah <rian.adriansyah@fifgroup.astra.co.id>
2025-09-17 12:00:18 +0700

    feat: fix table component and add sidebar

--- src/app/pages/application-status/application-status.component.ts
+++ src/app/pages/application-status/application-status.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { ApplicationStoreService } from '../../shared/services/application-store.service';
 import { Application } from '../../models/application.model';
 import { CommonModule} from '@angular/common';
+import { TableComponent } from '../../shared/components/table/table.component';
 
 @Component({
   selector: 'app-application-status',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    TableComponent],
   templateUrl: './application-status.component.html',
   styleUrl: './application-status.component.scss'
 })
