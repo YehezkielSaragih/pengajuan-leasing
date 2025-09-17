dfaf9e9383b083fc430c361fbd90d9f0f94acd9c
Yehezkiel Stephanus Austin <yehezkiel.stephanus@gmail.com>
2025-09-17 12:37:54 +0700

    feat: add scss form application

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './not-found.component.scss'
 })
 export class NotFoundComponent {
+  constructor(private router: Router) {}
 
+  goToHome(): void {
+    this.router.navigate(['/form-application']);
+  }
 }
